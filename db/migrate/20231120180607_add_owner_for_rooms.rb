d5a0e249744dc70bd69e5996fab1d54036329b96
Alexandr Seregin <seregin_a03@mail.ru>
2023-11-21 22:03:34 +0300

    add migration to rooms

--- db/migrate/20231120180607_add_owner_for_rooms.rb
+++ db/migrate/20231120180607_add_owner_for_rooms.rb
@@ -1,9 +1,14 @@
 class AddOwnerForRooms < ActiveRecord::Migration[7.0]
   def up
-    add_reference :rooms, :user, null: false, foreign_key: true
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+    add_reference :rooms, :user, references: :owner, null: false, foreign_key: true
+    add_column :rooms, :name, :string, default: ''
+    add_column :rooms, :password, :string, default: ''
+    add_column :rooms, :private, :boolean, null: false, default: false
   end
   def def
     remove_reference :rooms, :user, foreign_key: true
+    remove_column :rooms, :name
+    remove_column :rooms, :password
+    remove_column :rooms, :private
   end
 end
