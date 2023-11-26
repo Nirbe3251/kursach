c0be7cbb43155567f11089c539764394c871122e
Alexandr Seregin <seregin_a03@mail.ru>
2023-11-26 12:55:40 +0300

    add importmap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
 //= link_tree ../builds
+//= link_tree ../../../vendor/assets/stylesheets .css
+//= link_tree ../../../vendor/assets/javascript .js
 //= link application.css
